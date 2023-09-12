f9d24cbb862b054aa43c2fae3477ba05ecea0557
salamander297 <timaghostlvl@gmail.com>
2023-09-12 12:51:51 +0300

    первый коммит

---
+++ main.py
@@ -0,0 +1,29 @@
+# import math
+#
+# a = float(input("введите коэфицент 1:"))
+# b = float(input("введите коэфицент 2:"))
+# c = float(input("введите коэфицент 3:"))
+# D = b ** 2 - 4 * a * c
+# if D > 0:
+#     x1 = (-b + math.sqrt(D)) / (2 * a)
+#     x2 = (-b - math.sqrt(D)) / (2 * a)
+#     print(f"корни уравнения: x1 = {x1}, x2 = {x2}")
+# elif D == 0:
+#     x1 = -b / (2 * a)
+#     print("уравнение имеет один корень: x = ", x1)
+# else:
+#     print(" нет корней, расходимся")
+import math
+while True:
+    user_input = input("введите число")
+    if user_input == 'q':
+        break
+try:
+
+    number = int(input("введите число"))
+    if number % 2 == 0:
+        print("четное")
+    else:
+        print("не четное")
+except ValueError:
+    print("не катит , давай нормально")
